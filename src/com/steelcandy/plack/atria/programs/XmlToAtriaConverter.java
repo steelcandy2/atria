7dd4be43c74f0f9d7121a57b13706b841a88057b
James MacKay <jgm@steelcandy.org>
2023-08-28 17:44:01 -0400

    Updated copyright years.
    To account for the fairly substantial changes that I'm about to make.

--- src/com/steelcandy/plack/atria/programs/XmlToAtriaConverter.java
+++ src/com/steelcandy/plack/atria/programs/XmlToAtriaConverter.java
@@ -1,5 +1,5 @@
 /*
- Copyright (C) 2005 by James MacKay.
+ Copyright (C) 2005-2023 by James MacKay.
 
  This program is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
